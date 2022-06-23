87979cb3b854bbaf3c084301f8b3de8319f35abd
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-06-23 14:00:47 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,14 @@
+/*
+@author Nilanjan
+*/
+import java.util.*;  
+class User 
+{  
+public static void main(String[] args)  
+{  
+Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+System.out.print("Enter a string: ");  
+String str= sc.nextLine();              //reads string   
+System.out.print("You have entered: "+str);             
+}  
+}  
